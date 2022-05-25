150291f7f629c54779e67a5e4ec9d76daff56641
Tvibe <tvibe58@163.com>
2022-05-25 16:18:01 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': ['off']
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'space-before-function-paren': [2, 'always'],
     // 'space-before-function-paren': ['error', 'never']
   }
 }
